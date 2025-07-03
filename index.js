3ee2ba846bf099a09f1759f15c58b11b78d71a15
louis nguyen <louis.nguyen550@gmail.com>
2025-07-03 19:24:10 -0400

    adding password options

--- index.js
+++ index.js
@@ -4,12 +4,61 @@ const includeUppercase = true;
 const includeNumbers = true;
 const includeSymbols = true;
 
-// generate password function
-generatePassword = (length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) => {
-
-}
-
 // call function
 const password = generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
 
 console.log(`Generated password: ${password}`);
+
+// generate password function
+generatePassword = (length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) => {
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()-_+=";
+
+    // declare empty strings
+    let allowedChars = "";
+    let password = "";
+
+    // concating strings
+    if (includeLowercase === true)
+    {
+        allowedChars += lowercaseChars
+    }
+    else
+    {
+        allowedChars += "";
+    }
+
+    if (includeUppercase === true)
+    {
+        allowedChars += uppercaseChars
+    }
+    else
+    {
+        allowedChars += "";
+    }
+
+    if (includeNumbers === true)
+    {
+        allowedChars += numberChars;
+    }
+    else
+    {
+        allowedChars += "";
+    }
+
+    if (includeSymbols === true)
+    {
+        allowedChars += symbolChars;
+    }
+    else
+    {
+        allowedChars += "";
+    }
+
+    console.log(allowedChars);
+
+    return '';
+}
