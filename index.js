59d4f23eb3a43ae281fc9b69f5af1d3f63d850a7
louis nguyen <louis.nguyen550@gmail.com>
2025-07-03 19:15:56 -0400

    functions created

---
+++ index.js
@@ -0,0 +1,15 @@
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+// generate password function
+generatePassword = (length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) => {
+
+}
+
+// call function
+const password = generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+
+console.log(`Generated password: ${password}`);
